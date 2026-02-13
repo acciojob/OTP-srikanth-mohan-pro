27235be011295691650e75da310b2c3587a4016f
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-13 12:50:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const codes=document.querySelectorAll(".code");
+codes.forEach((code,index)=>{
+	code.addEventListener("input",()=>{
+		if(code.value.length===1&&index<codes.length-1){
+			codes[index+1].focus();
+		}
+	});
+	code.addEventListener("keydown",(e)=>{
+		if(e.key==="Backspace"){
+			if(code.value===""&&index>0){
+				codes[index-1].focus();
+			}
+		}
+	});
+});
